2d68fa1236e0f2dc10c78c3747427afa636b6857
emirhanwhy <emirhanwhy@gmail.com>
2018-10-26 00:54:49 +0300

    Add files via upload

---
+++ disqus-load-more.js
@@ -0,0 +1,16 @@
+// Requires jQuery of course.
+$(document).ready(function() {
+    $('.show-comments').on('click', function(){
+          var disqus_shortname = 'indirgo'; // Replace this value with *your* username.
+
+          // ajax request to load the disqus javascript
+          $.ajax({
+                  type: "GET",
+                  url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+                  dataType: "script",
+                  cache: true
+          });
+          // hide the button once comments load
+          $(this).fadeOut();
+    });
+});
